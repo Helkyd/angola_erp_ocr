72631918c628d94caab0970579cd4996628d9320
hcesar <hcesar@gmail.com>
2023-09-20 16:16:49 +0100

    Fix

--- angola_erp_ocr/api/util.py
+++ angola_erp_ocr/api/util.py
@@ -7020,6 +7020,7 @@ def agt_lgt(dlinumber):
 
 		else:
 			#return out #.decode("utf-8")
+			print (out.decode("utf-8"))
 			tmpficheiroPDF = out.decode("utf-8").split('\n')[12]
 			ficheiroPDF = tmpficheiroPDF[tmpficheiroPDF.find('VERIFICAR PASTA ')+16:]
 
